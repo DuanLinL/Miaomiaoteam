c1b2537dc18968a081bc61dfa6627fb685002cfb
forever_duoduooo <1518007648@qq.com>
2023-04-10 00:03:47 +0800

    更新了一点但没写好，总之就是先更新下

--- Miaoproject/miaoproject/src/components/TaskList.vue
+++ Miaoproject/miaoproject/src/components/TaskList.vue
@@ -1,43 +1,91 @@
 <template>
-  <div class="List">
-    <div class="HeadLine">
-      <div class="left">
-        <img src="@/assets/icon/kitty.png" alt="Logo" class="logo">
-        <span class="title">我的一天</span>
-      </div>
+  <div class="TaskBox">
+    <div class="InTaskBox"
+         v-for="(list, index) in BoxLists"
+         :key="list.id"
+         v-bind:class="{'TaskBoxNow':index === actionBoxNum}"
+    >
+      {{ list.name }}
     </div>
-
-    <ul>
-      <li class="line">
-        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">
-        吃饭
-      </li>
-      <li class="line">
-        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">
-        打游戏
-      </li>
-      <li class="line">
-        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">
-      </li>
-    </ul>
   </div>
+  <!--    <div class="HeadLine">-->
+  <!--      <div>-->
+  <!--        <img src="@/assets/icon/kitty.png" alt="Logo" class="logo">-->
+  <!--        <span class="title">我的一天</span>-->
+  <!--      </div>-->
+  <!--    </div>-->
+
+  <!--    <ul>-->
+  <!--      <li class="line">-->
+  <!--        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">-->
+  <!--        吃饭-->
+  <!--      </li>-->
+  <!--      <li class="line">-->
+  <!--        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">-->
+  <!--        打游戏-->
+  <!--      </li>-->
+  <!--      <li class="line">-->
+  <!--        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">-->
+  <!--      </li>-->
+  <!--    </ul>-->
+
 
 </template>
 
 <script>
 export default {
-  name: "TaskList.vue"
+  name: "TaskList.vue",
+  data() {
+    return {
+      BoxLists: [
+        {id: 1, name: '我的一天'},
+        {id: 2, name: '重要'},
+        {id: 3, name: '全部任务'}
+      ],
+    }
+  },
+  props: ['actionBoxNum']
 }
+
+
 </script>
 
 <style scoped>
-.List {
+/*.List {*/
+/*  background-color: #F2F2F2;*/
+/*  width: 100%;*/
+/*  padding: 1rem;*/
+/*  box-sizing: border-box;*/
+/*  height: 100%;*/
+/*  overflow-y: auto;*/
+/*}*/
+.TaskBox {
   background-color: #F2F2F2;
   width: 100%;
-  padding: 1rem;
-  box-sizing: border-box;
   height: 100%;
-  overflow-y: auto;
+  transition: all 0.3s;
+  position: relative;
+  /*opacity: 0;*/
+  z-index: 0;
+  pointer-events: none;
+}
+
+.InTaskBox {
+  /*background-color: #ea0000;*/
+  color: #F2F2F2;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+
+}
+
+.TaskBoxNow {
+  /*opacity: 1;*/
+  z-index: 1;
+  color: #000000;
+  font-size: 25px;
+  pointer-events: all;
+  font-weight: bold; /* 设置字体粗细 */
 }
 
 .HeadLine {
