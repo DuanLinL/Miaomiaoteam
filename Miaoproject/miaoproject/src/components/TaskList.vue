4b64f1b627d7f70d96774bc6bcf7ea7f45ed075a
forever_duoduooo <1518007648@qq.com>
2023-04-12 23:34:48 +0800

    Revert "更新tasklist by llq"
    This reverts commit 6774cf6caa818af2973dce40e8e4f4deaabe2e64.

--- Miaoproject/miaoproject/src/components/TaskList.vue
+++ Miaoproject/miaoproject/src/components/TaskList.vue
@@ -1,42 +1,34 @@
 <template>
   <div class="TaskBox">
-
-        <div class="HeadBox" v-if="actionBoxNum === 0">
-            <div class="TaskInput">
-              <input type="text">
-              <div class="Input_but">按钮</div>
-            </div>
-            <div class="Task_List">
-              <ul>
-                <li v-for="list in TaskLists_1" :key= "list">{{ list.name }}</li>   
-              </ul>
-            </div>
-        </div>
-        <div class="HeadBox" v-if="actionBoxNum === 1">
-          <div class="TaskInput">
-              <input type="text">
-              <div class="Input_but">按钮</div>
-            </div>
-            <div class="Task_List">
-              <ul>
-                <li v-for="list in TaskLists_2" :key= "list">{{ list.name }}</li>   
-              </ul>
-            </div>
-        </div>
-        <div class="HeadBox" v-if="actionBoxNum === 2">
-          <div class="TaskInput">
-              <input type="text">
-              <div class="Input_but">按钮</div>
-            </div>
-            <div class="Task_List">
-              <ul>
-                <li v-for="list in TaskLists_3" :key= "list">{{ list.name }}</li>   
-              </ul>
-            </div>
-        </div>
-
-       
+    <div class="InTaskBox"
+         v-for="(list, index) in BoxLists"
+         :key="list.id"
+         v-bind:class="{'TaskBoxNow':index === actionBoxNum}"
+    >
+      {{ list.name }}
+    </div>
   </div>
+  <!--    <div class="HeadLine">-->
+  <!--      <div>-->
+  <!--        <img src="@/assets/icon/kitty.png" alt="Logo" class="logo">-->
+  <!--        <span class="title">我的一天</span>-->
+  <!--      </div>-->
+  <!--    </div>-->
+
+  <!--    <ul>-->
+  <!--      <li class="line">-->
+  <!--        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">-->
+  <!--        吃饭-->
+  <!--      </li>-->
+  <!--      <li class="line">-->
+  <!--        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">-->
+  <!--        打游戏-->
+  <!--      </li>-->
+  <!--      <li class="line">-->
+  <!--        <img src="@/assets/icon/icon6.png" alt="Logo" class="logo">-->
+  <!--      </li>-->
+  <!--    </ul>-->
+
 
 </template>
 
@@ -45,18 +37,10 @@ export default {
   name: "TaskList.vue",
   data() {
     return {
-      TaskLists_1: [
-        {name:111},
-        {name:222},
-        {name:333}
-      ],
-      TaskLists_2: [
-        {name:111},
-        {name:222},
-      ],
-      TaskLists_3: [
-        {name:111},
-
+      BoxLists: [
+        {id: 1, name: '我的一天'},
+        {id: 2, name: '重要'},
+        {id: 3, name: '全部任务'}
       ],
     }
   },
@@ -67,77 +51,41 @@ export default {
 </script>
 
 <style scoped>
-
+/*.List {*/
+/*  background-color: #F2F2F2;*/
+/*  width: 100%;*/
+/*  padding: 1rem;*/
+/*  box-sizing: border-box;*/
+/*  height: 100%;*/
+/*  overflow-y: auto;*/
+/*}*/
 .TaskBox {
   background-color: #F2F2F2;
   width: 100%;
   height: 100%;
+  transition: all 0.3s;
   position: relative;
+  /*opacity: 0;*/
+  z-index: 0;
+  pointer-events: none;
 }
 
-  .HeadBox{
+.InTaskBox {
+  /*background-color: #ea0000;*/
+  color: #F2F2F2;
   width: 100%;
   height: 100%;
   position: absolute;
-  display: flex;
-  flex-direction: column;
+
 }
 
-.HeadBox .TaskInput{
-  
-  width: 100%;
-  flex: 5;
-  display: flex;
-  align-items: center;
-  
-}
-
-.TaskInput input{
-  width: 75%;
-  height: 28%;
-  font-size: 1.1rem; /* 添加此行以调整字体大小 */
-  font-family: "Arial", sans-serif; /* 添加此行以更改字体 */
-  display: block;
-  margin-left: 6%;
-  border-radius: 10px;
-  border:2px solid #1f1f1f;
-  
-}
-
-.Input_but{
-  background-color: #e6e603;
-  width: 9%;
-  height: 32%;
-  margin-left: 5%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border:2px solid #1f1f1f;
-  border-radius: 10px;
-}
-
-.HeadBox .Task_List{
-  width: 100%;
-  flex: 25;
-  list-style-type: none;
-}
-
-ul{
-  margin-top: 3%;
-}
-
-.HeadBox .Task_List li{
-  list-style-type: none;
-  background-color: #dadb9e;
-  margin-top: 2%;
-  margin-left: 1%;
-  margin-right: 4%;
-  border-radius: 5px;
-  min-height: 40px;
-  font-size: 1.3rem;
-  text-indent: 1.2em;
-  display: flex;
-  align-items: center;
+.TaskBoxNow {
+  /*opacity: 1;*/
+  z-index: 1;
+  color: #000000;
+  font-size: 25px;
+  pointer-events: all;
+  font-weight: bold; /* 设置字体粗细 */
 }
 
 .HeadLine {
